1e0edf4e350fb2a60dbb0651150e216a5d68957c
maccotsan <maccotsan@gmail.com>
2016-10-20 18:50:57 +0900

    change travis config.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!bash
 
-git worktree add pages gh-pages
-go build
-./mhxr
-cd ./pages
-git commit -a -m "Update today schedule."
+#git worktree add pages gh-pages
+#./mhxr
+#cd ./pages
+#git commit -a -m "Update today schedule."
 git push --force --quiet "https://${GH_TOKEN}@github.com/maccotsan/mhxr.git" gh-pages:gh-pages > /dev/null 2>&1
